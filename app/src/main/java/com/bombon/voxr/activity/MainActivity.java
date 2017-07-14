10b6c294cdf4a3106aff8ce27174747c0a3e5dec
Vaughn Armada <bonarmada@gmail.com>
2017-07-14 20:30:23 +0800

    init dependency injection

--- app/src/main/java/com/bombon/voxr/activity/MainActivity.java
+++ app/src/main/java/com/bombon/voxr/activity/MainActivity.java
@@ -4,6 +4,7 @@ package com.bombon.voxr.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.bombon.voxr.App;
 import com.bombon.voxr.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((App)getApplication()).getComponent().inject(this);
     }
 }
